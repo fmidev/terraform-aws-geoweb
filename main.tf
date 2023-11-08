45a8ab05f4a82a2bc9239dfcf7b37e2c0d46ec74
Jussi Liukkonen <jussi.liukkonen@crowdcollective.com>
2023-11-08 10:17:43 +0200

    Remove unneccessary ebs sa permission

--- main.tf
+++ main.tf
@@ -290,7 +290,7 @@ module "ebs_csi_irsa_role" {
   oidc_providers = {
     ex = {
       provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa", "default:default"]
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
     }
   }
 }
